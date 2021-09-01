595ea7a9cdef9a5d7ddd4911761fb1c0765c97eb
gunner10z <24jzweiback@athenian.org>
2021-09-01 15:52:12 -0700

    first attempt

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,7 +4,25 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Add some code here!
+        System.out.print("What's your name, friend: ");
+        String name = scan.nextLine();
+        System.out.println("Nice to meet you, "+name);
+
+        System.out.print("What's your favorite color: ");
+        String color = scan.nextLine();
+        System.out.println("My favorite color is also "+color);
+
+        System.out.print("What is your favorite book: ");
+        String book = scan.nextLine();
+        System.out.println("Oh, I hate "+book);
+
+        System.out.print("What is your favorite subject: ");
+        String subject = scan.nextLine();
+        System.out.println("I also love "+subject);
+
+        System.out.print("What's your favorite programming language" );
+        String lang = scan.nextLine();
+        System.out.println("Both C and " +lang+ " suck.");
 
         scan.close();
     }
